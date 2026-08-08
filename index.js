aca1b6577b005d5a35581580637068800d9cab59
tynot484 <cr7messs30@gmail.com>
2026-08-08 12:28:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const config = {
     host: process.env.SERVER_HOST || 'node-de-free-01.tickhosting.com',
     port: parseInt(process.env.SERVER_PORT || '50589'),
     username: process.env.BOT_USERNAME || 'dma9',
-    version: '1.21.1',
+    version: false,
     auth: process.env.AUTH_TYPE || 'offline'
 };
 
